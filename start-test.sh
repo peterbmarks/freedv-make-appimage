57b1cdc17944047ef3347b082b77763c8634891d
Peter Marks <peterbmarks@gmail.com>
2025-06-09 18:10:47 +1000

    Add a test script

--- start-test.sh
+++ start-test.sh
@@ -1,5 +1,5 @@
-#!/bin/bash -e
-cd "$APPDIR/freedv-gui"
+#!/bin/bash
+cd "$HOME/freedv-make-appimage/FreeDV.AppDir/freedv-gui"
 . ./rade-venv/bin/activate
 cd build_linux
 PYTHONPATH="$(pwd)/rade_src:$PYTHONPATH" src/freedv $1 $2
