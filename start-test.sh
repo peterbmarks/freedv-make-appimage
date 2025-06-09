da2178648067fc12886361e8d12bd404a8da9d6b
Peter Marks <peterbmarks@gmail.com>
2025-06-09 18:45:49 +1000

    hint on testing with the mounted file system

--- start-test.sh
+++ start-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Test the build using the AppDir tree
 APPDIR="$HOME/freedv-make-appimage/FreeDV.AppDir"
+
+# ./FreeDV-x86_64.AppImage --appimage-mount 
+#APPDIR=/tmp/.mount_FreeDVc5qK7Y
 echo "### appdir=$APPDIR"
 source "$APPDIR/freedv-gui/rade-venv/bin/activate"
 PYTHONPATH="$APPDIR/freedv-gui/build_linux/rade_src:$PYTHONPATH" "$APPDIR/freedv-gui/build_linux/src/freedv" $1 $2
