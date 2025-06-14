64506f618d4098123bbd2a951a38d78cd220107f
Peter Marks <peterbmarks@gmail.com>
2025-06-14 18:56:50 +1000

    getting there

--- freedv-make-appimage.sh
+++ freedv-make-appimage.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -e
 
 APPNAME="FreeDV"
-DESTDIR="$APPNAME.AppDir"
+APPDIR="$APPNAME.AppDir"
 BUILDDIR="../freedv-rade/freedv-gui"
 
 # Change to the directory where this script is located
 cd "$(dirname "$(realpath "$0")")"
 
-if [ -d "$DESTDIR" ]; then
-    echo "Deleting $DESTDIR..."
-    rm -rf "$DESTDIR"
+if [ -d "$APPDIR" ]; then
+    echo "Deleting $APPDIR..."
+    rm -rf "$APPDIR"
 else
-    echo "$DESTDIR does not exist."
+    echo "$APPDIR does not exist."
 fi
 
 echo "Bundle dependencies..."
@@ -25,7 +25,7 @@ fi
 ./linuxdeploy-x86_64.AppImage \
 --executable /usr/bin/python3 \
 --executable ~/freedv-rade/freedv-gui/build_linux/src/freedv \
---appdir "$DESTDIR" \
+--appdir "$APPDIR" \
 --icon-file FreeDVIcon.png \
 --custom-apprun=AppRun.sh \
 --desktop-file FreeDV.desktop
@@ -46,7 +46,7 @@ cd -
 
 echo "Fix venv python links..."
 echo "Now in $(pwd)"
-cd "$DESTDIR/rade-venv/bin"
+cd "$APPDIR/rade-venv/bin"
 echo "Now in $(pwd)"
 ln -s -f ../../usr/bin/python3 python
 ln -s -f ../../usr/bin/python3 python3
@@ -54,17 +54,18 @@ ln -s -f ../../usr/bin/python3 python3.12
 cd - # back to the previous directory
 echo "### Now in $(pwd)"
 
-# TODO copy the models and symlink
+# Copy the models and symlink
 echo "Copying rade_src..."
 # ls freedv-rade/freedv-gui/build_linux/rade_src/model
 # model05/        model17/        model18/        model19/        model19_check3/ model_bbfm_01/  
-cp -r "$BUILDDIR/build_linux/rade_src" "$DESTDIR/."
-cd "$DESTDIR"
-ln -s "$BUILDDIR/build_linux/rade_src/model19_check3" "$DESTDIR/."
+cp -r "$BUILDDIR/build_linux/rade_src" "$APPDIR/."
+cd "$APPDIR"
+ln -s "$APPDIR/rade_src/model19_check3" "model19_check3"
+cd -
 
 # Create the output
 ./linuxdeploy-x86_64.AppImage \
---appdir "$DESTDIR" \
+--appdir "$APPDIR" \
 --output appimage
 
 echo "Done"
