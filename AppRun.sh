125be9ed34f5cb5f796af95f72db4336058fa022
Peter Marks <peterbmarks@gmail.com>
2025-06-16 09:11:35 +1000

    export paths and check python in AppRun

--- AppRun.sh
+++ AppRun.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
-echo "In AppRun"
-PATH="$APPDIR/usr/bin:$PATH"
-echo "PATH=$PATH"
-PYTHONPATH="$APPDIR/rade_src:$APPDIR/rade-venv/lib/python3.12/site-packages:$PYTHONPATH"
-echo "PYTHONPATH=$PYTHONPATH"
+echo "In AppImage AppRun"
 export LD_LIBRARY_PATH="${APPIMAGE_LIBRARY_PATH}:${APPDIR}/usr/lib:${LD_LIBRARY_PATH}"
+export PATH="$APPDIR/usr/bin"
+echo "PATH=$PATH"
+export PYTHONPATH="$APPDIR/rade_src:$APPDIR/rade-venv/lib/python3.12/site-packages:$PYTHONPATH"
+echo "PYTHONPATH=$PYTHONPATH"
+python3 -c "print('Hello, world from python!')"
+echo "##### About to import radae_rxe"
+python3 -c "import radae_rxe"
 "$APPDIR/usr/bin/freedv" $1 $2
 deactivate
