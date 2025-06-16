b889b445f5693f12b02a876579dc85cbb25ec0fe
Peter Marks <peterbmarks@gmail.com>
2025-06-16 15:37:38 +1000

    Looks like this won't work as there's no fuse to mount the python appimage

--- freedv-make-appimage.sh
+++ freedv-make-appimage.sh
@@ -13,16 +13,6 @@ if [ -d "$APPDIR" ]; then
 else
     echo "$APPDIR does not exist."
 fi
-mkdir -p "$APPDIR/usr/bin"
-
-echo "Get python appimage..."
-if test -f python3.14.0b1-cp314-cp314-manylinux2014_x86_64.AppImage; then
-  echo "python appimage exists"
-else
-  wget https://github.com/niess/python-appimage/releases/download/python3.14/python3.14.0b1-cp314-cp314-manylinux2014_x86_64.AppImage
-  chmod +x python3.14.0b1-cp314-cp314-manylinux2014_x86_64.AppImage
-fi
-cp python3.14.0b1-cp314-cp314-manylinux2014_x86_64.AppImage "$APPDIR/usr/bin/python3"
 
 
 echo "Bundle dependencies..."
@@ -34,16 +24,27 @@ else
 fi
 
 ./linuxdeploy-x86_64.AppImage \
-#--executable /usr/bin/python3 \
 --executable ~/freedv-rade/freedv-gui/build_linux/src/freedv \
 --appdir "$APPDIR" \
 --icon-file FreeDVIcon.png \
 --custom-apprun=AppRun.sh \
 --desktop-file FreeDV.desktop
 
+echo "Get python appimage..."
+export PYTHONFILENAME="python3.12.11-cp312-cp312-manylinux_2_28_x86_64.AppImage"
+export PYTHONAPPIMAGEURL="https://github.com/niess/python-appimage/releases/download/python3.12/python3.12.11-cp312-cp312-manylinux_2_28_x86_64.AppImage"
+if test -f "$PYTHONFILENAME"; then
+  echo "python appimage exists"
+else
+  wget "$PYTHONAPPIMAGEURL"
+  chmod +x "$PYTHONFILENAME"
+fi
+mkdir -p "$APPDIR/bin"
+cp "$PYTHONFILENAME" "$APPDIR/bin/python3.AppImage"
+
 # create the virtual environment (copied from Brian's build script)
 cd "$APPDIR"
-usr/bin/python3 -m venv rade-venv # || { echo "ERROR: create venv failed"; exit 1; }
+bin/python3.AppImage -m venv rade-venv # || { echo "ERROR: create venv failed"; exit 1; }
 # Activate it
 source rade-venv/bin/activate # || { echo "ERROR: activate venv failed"; exit 1; }
 
@@ -59,9 +60,9 @@ echo "Fix venv python links..."
 echo "Now in $(pwd)"
 cd "$APPDIR/rade-venv/bin"
 echo "Now in $(pwd)"
-ln -s -f ../../usr/bin/python3 python
-ln -s -f ../../usr/bin/python3 python3
-ln -s -f ../../usr/bin/python3 python3.12
+ln -s -f ../../bin/python3.AppImage python
+ln -s -f ../../bin/python3.AppImage python3
+ln -s -f ../../bin/python3.AppImage python3.12
 cd - # back to the previous directory
 echo "### Now in $(pwd)"
 
@@ -77,6 +78,8 @@ cd -
 # Create the output
 ./linuxdeploy-x86_64.AppImage \
 --appdir "$APPDIR" \
+--icon-file FreeDVIcon.png \
+--desktop-file FreeDV.desktop \
 --output appimage
 
 echo "Done"
