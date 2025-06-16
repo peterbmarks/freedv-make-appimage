a6f22e149d35e06d0cc90074603baeea07b1e073
Peter Marks <peterbmarks@gmail.com>
2025-06-16 12:48:43 +1000

    more AppRun debugging

--- AppRun.sh
+++ AppRun.sh
@@ -5,9 +5,12 @@ export PATH="$APPDIR/usr/bin:$APPDIR/rade-venv/bin"
 echo "PATH=$PATH"
 export PYTHONPATH="$APPDIR/rade_src:$APPDIR/rade-venv/lib/python3.12/site-packages:$PYTHONPATH"
 echo "PYTHONPATH=$PYTHONPATH"
-python3 -c "print('Hello, world from python!')"
+"$APPDIR/rade-venv/bin/python3" -c "print('Hello, world from python!')"
+echo "##### About to activate venv"
+cd "$APPDIR"
+. ./rade-venv/bin/activate
 echo "##### About to import radae_rxe"
-python3 -c "import radae_rxe"
+"$APPDIR/rade-venv/bin/python3" -c "import radae_rxe"
 echo "#### after import"
 "$APPDIR/usr/bin/freedv" $1 $2
 deactivate
